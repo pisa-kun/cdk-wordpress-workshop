efb52446f54110e960cf49bc1bf91b5996728dce
pisa-kun <pisa0401mst@gmail.com>
2024-03-31 01:38:45 +0900

    ec2の宣言

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# Welcome to your CDK TypeScript project
+# aws cdkでwordpressサーバーのデプロイ
 
-You should explore the contents of this project. It demonstrates a CDK app with an instance of a stack (`CdkWordPressWorkShopStack`)
-which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
+https://catalog.workshops.aws/typescript-and-cdk-for-beginner/ja-JP/40-cdk-introduction/10-create-project/20-structure
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+### 
 
-## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+### 
+
+
+### 参考
+
--- lib/cdk-word_press-work_shop-stack.ts
+++ lib/cdk-word_press-work_shop-stack.ts
@@ -2,18 +2,26 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
 import { Construct } from 'constructs';
 
 export class CdkWordPressWorkShopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWordPressWorkShopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // vpcの宣言
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
     });
 
-    const topic = new sns.Topic(this, 'CdkWordPressWorkShopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // AWS Linux2
+    new ec2.Instance(this, "WordPressServer1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+    });
   }
 }
